61c859ed6b8aac9bd98f45f18404568ba8223207
Jan Filipowski <jachuf@gmail.com>
2015-08-17 16:15:46 +0200

    Client can pass it's banner

--- lib/json_bunny.rb
+++ lib/json_bunny.rb
@@ -5,8 +5,23 @@ require 'json_bunny/channel'
 
 module JsonBunny
   class Connection
-    def initialize(connection_string)
-      @connection = ::Bunny.new(connection_string)
+    def initialize(connection_string, connection_banner=nil)
+      properties = {
+        :capabilities => {
+          :publisher_confirms           => true,
+          :consumer_cancel_notify       => true,
+          :exchange_exchange_bindings   => true,
+          :"basic.nack"                 => true,
+          :"connection.blocked"         => true,
+          # See http://www.rabbitmq.com/auth-notification.html
+          :authentication_failure_close => true
+        },
+        :product      => connection_banner || "Bunny",
+        :platform     => ::RUBY_DESCRIPTION,
+        :version      => Bunny::VERSION,
+        :information  => "http://rubybunny.info"
+      }
+      @connection = ::Bunny.new(connection_string, properties: properties)
     end
 
     def start
